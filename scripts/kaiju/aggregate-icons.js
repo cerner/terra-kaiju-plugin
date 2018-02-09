672bd75000ef3d85f216595a939d255b3109e899
Stephen Esser <Stephen.Esser@gmail.com>
2018-02-09 15:42:23 -0600

    Updated - Aggregating the icons will generate an icon map (#12)
    * Updated - Aggregating the icons will generate an icon map
    
    * Remove icon mapping quotes
    
    * Icon map shorthand syntax

--- scripts/kaiju/aggregate-icons.js
+++ scripts/kaiju/aggregate-icons.js
@@ -3,16 +3,41 @@ const fs = require('fs');
 const glob = require('glob');
 const template = require('./template');
 
+/**
+ * Indents a string.
+ * @param {string} string - The string to indent.
+ * @param {number} count - The number of indentations.
+ * @return {string} - A string with the specified indentation.
+ */
+const indent = (string, count) => (
+  string.replace(/^(?!\s*$)/mg, (' ').repeat(count))
+);
+
+const imports = [];
+const iconMap = [];
 glob.sync('node_modules/terra-icon/lib/icon/*.js').forEach((file) => {
   const fileName = file.split('/').pop();
   const iconName = fileName.substring(0, fileName.lastIndexOf('.'));
   const titleCase = `${iconName.split('Icon').pop().replace(/([A-Z])/g, ' $1').trim()} Icon`;
+  const importFrom = `terra-icon/lib/icon/${iconName}`;
+
+  // Build out the icon map.
+  iconMap.push(`${iconName},`);
+  imports.push(`import ${iconName} from '${importFrom}';`);
 
   fs.writeFileSync(`kaiju/icons/${iconName}.json`, JSON.stringify({
     ...template,
     name: iconName,
     display: titleCase,
     description: titleCase,
-    import_from: `terra-icon/lib/icon/${iconName}`,
+    import_from: importFrom,
   }, null, 2));
 });
+
+// Generate the icon map file.
+const mapString = `\n\nconst iconMap = {\n${indent(iconMap.join('\n'), 2)}\n};`;
+
+fs.writeFileSync(
+  'iconMap.js',
+  `${imports.join('\n')}${mapString}\n\nexport default iconMap;\n`,
+);
