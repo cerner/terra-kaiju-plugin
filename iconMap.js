f194189cc2507b85f8d035571afe721bcf22f8c3
Esser,Stephen <Stephen.Esser@Cerner.com>
2018-02-09 10:37:56 -0600

    Updated - Aggregating the icons will generate an icon map

---
+++ iconMap.js
@@ -0,0 +1,353 @@
+import IconAbnormal from 'terra-icon/lib/icon/IconAbnormal';
+import IconAdd from 'terra-icon/lib/icon/IconAdd';
+import IconAddPerson from 'terra-icon/lib/icon/IconAddPerson';
+import IconAlert from 'terra-icon/lib/icon/IconAlert';
+import IconAllergy from 'terra-icon/lib/icon/IconAllergy';
+import IconAnalytics from 'terra-icon/lib/icon/IconAnalytics';
+import IconAnnouncement from 'terra-icon/lib/icon/IconAnnouncement';
+import IconArchive from 'terra-icon/lib/icon/IconArchive';
+import IconAttachment from 'terra-icon/lib/icon/IconAttachment';
+import IconAudio from 'terra-icon/lib/icon/IconAudio';
+import IconAvailable from 'terra-icon/lib/icon/IconAvailable';
+import IconAway from 'terra-icon/lib/icon/IconAway';
+import IconBookmark from 'terra-icon/lib/icon/IconBookmark';
+import IconBriefcase from 'terra-icon/lib/icon/IconBriefcase';
+import IconBusy from 'terra-icon/lib/icon/IconBusy';
+import IconCalculator from 'terra-icon/lib/icon/IconCalculator';
+import IconCalendar from 'terra-icon/lib/icon/IconCalendar';
+import IconCamera from 'terra-icon/lib/icon/IconCamera';
+import IconCancel from 'terra-icon/lib/icon/IconCancel';
+import IconCaretDown from 'terra-icon/lib/icon/IconCaretDown';
+import IconCaretLeft from 'terra-icon/lib/icon/IconCaretLeft';
+import IconCaretRight from 'terra-icon/lib/icon/IconCaretRight';
+import IconCaretUp from 'terra-icon/lib/icon/IconCaretUp';
+import IconChecklist from 'terra-icon/lib/icon/IconChecklist';
+import IconCheckmark from 'terra-icon/lib/icon/IconCheckmark';
+import IconChevron from 'terra-icon/lib/icon/IconChevron';
+import IconChevronDown from 'terra-icon/lib/icon/IconChevronDown';
+import IconChevronLeft from 'terra-icon/lib/icon/IconChevronLeft';
+import IconChevronRight from 'terra-icon/lib/icon/IconChevronRight';
+import IconChevronUp from 'terra-icon/lib/icon/IconChevronUp';
+import IconClear from 'terra-icon/lib/icon/IconClear';
+import IconClipboard from 'terra-icon/lib/icon/IconClipboard';
+import IconClock from 'terra-icon/lib/icon/IconClock';
+import IconClose from 'terra-icon/lib/icon/IconClose';
+import IconComment from 'terra-icon/lib/icon/IconComment';
+import IconCommit from 'terra-icon/lib/icon/IconCommit';
+import IconComplete from 'terra-icon/lib/icon/IconComplete';
+import IconCompose from 'terra-icon/lib/icon/IconCompose';
+import IconCritical from 'terra-icon/lib/icon/IconCritical';
+import IconDevice from 'terra-icon/lib/icon/IconDevice';
+import IconDeviceAlert from 'terra-icon/lib/icon/IconDeviceAlert';
+import IconDeviceCheck from 'terra-icon/lib/icon/IconDeviceCheck';
+import IconDiamond from 'terra-icon/lib/icon/IconDiamond';
+import IconDocuments from 'terra-icon/lib/icon/IconDocuments';
+import IconDoNotDisturb from 'terra-icon/lib/icon/IconDoNotDisturb';
+import IconDoorOpen from 'terra-icon/lib/icon/IconDoorOpen';
+import IconDown from 'terra-icon/lib/icon/IconDown';
+import IconDownload from 'terra-icon/lib/icon/IconDownload';
+import IconDue from 'terra-icon/lib/icon/IconDue';
+import IconDueSoon from 'terra-icon/lib/icon/IconDueSoon';
+import IconEdit from 'terra-icon/lib/icon/IconEdit';
+import IconEllipses from 'terra-icon/lib/icon/IconEllipses';
+import IconEnvelope from 'terra-icon/lib/icon/IconEnvelope';
+import IconError from 'terra-icon/lib/icon/IconError';
+import IconExclamation from 'terra-icon/lib/icon/IconExclamation';
+import IconExpandLess from 'terra-icon/lib/icon/IconExpandLess';
+import IconExpandMore from 'terra-icon/lib/icon/IconExpandMore';
+import IconFeatured from 'terra-icon/lib/icon/IconFeatured';
+import IconFeaturedOff from 'terra-icon/lib/icon/IconFeaturedOff';
+import IconFeaturedOutline from 'terra-icon/lib/icon/IconFeaturedOutline';
+import IconFirst from 'terra-icon/lib/icon/IconFirst';
+import IconFlag from 'terra-icon/lib/icon/IconFlag';
+import IconFlowsheet from 'terra-icon/lib/icon/IconFlowsheet';
+import IconFolder from 'terra-icon/lib/icon/IconFolder';
+import IconForward from 'terra-icon/lib/icon/IconForward';
+import IconFunnel from 'terra-icon/lib/icon/IconFunnel';
+import IconGapChecking from 'terra-icon/lib/icon/IconGapChecking';
+import IconGlasses from 'terra-icon/lib/icon/IconGlasses';
+import IconHelp from 'terra-icon/lib/icon/IconHelp';
+import IconHelpInverse from 'terra-icon/lib/icon/IconHelpInverse';
+import IconHigh from 'terra-icon/lib/icon/IconHigh';
+import IconHighPriority from 'terra-icon/lib/icon/IconHighPriority';
+import IconHospital from 'terra-icon/lib/icon/IconHospital';
+import IconHouse from 'terra-icon/lib/icon/IconHouse';
+import IconImage from 'terra-icon/lib/icon/IconImage';
+import IconImplant from 'terra-icon/lib/icon/IconImplant';
+import IconIncomingCall from 'terra-icon/lib/icon/IconIncomingCall';
+import IconIncomplete from 'terra-icon/lib/icon/IconIncomplete';
+import IconInformation from 'terra-icon/lib/icon/IconInformation';
+import IconInformationInverse from 'terra-icon/lib/icon/IconInformationInverse';
+import IconIPass from 'terra-icon/lib/icon/IconIPass';
+import IconItalicI from 'terra-icon/lib/icon/IconItalicI';
+import IconKnurling from 'terra-icon/lib/icon/IconKnurling';
+import IconLast from 'terra-icon/lib/icon/IconLast';
+import IconLeft from 'terra-icon/lib/icon/IconLeft';
+import IconLeftPane from 'terra-icon/lib/icon/IconLeftPane';
+import IconLightbulb from 'terra-icon/lib/icon/IconLightbulb';
+import IconLink from 'terra-icon/lib/icon/IconLink';
+import IconList from 'terra-icon/lib/icon/IconList';
+import IconLookback from 'terra-icon/lib/icon/IconLookback';
+import IconLow from 'terra-icon/lib/icon/IconLow';
+import IconMax from 'terra-icon/lib/icon/IconMax';
+import IconMaximize from 'terra-icon/lib/icon/IconMaximize';
+import IconMenu from 'terra-icon/lib/icon/IconMenu';
+import IconMerge from 'terra-icon/lib/icon/IconMerge';
+import IconMicrophone from 'terra-icon/lib/icon/IconMicrophone';
+import IconMicrophoneDisabled from 'terra-icon/lib/icon/IconMicrophoneDisabled';
+import IconMin from 'terra-icon/lib/icon/IconMin';
+import IconMinimize from 'terra-icon/lib/icon/IconMinimize';
+import IconMinus from 'terra-icon/lib/icon/IconMinus';
+import IconMissedCall from 'terra-icon/lib/icon/IconMissedCall';
+import IconModified from 'terra-icon/lib/icon/IconModified';
+import IconNext from 'terra-icon/lib/icon/IconNext';
+import IconNoSignal from 'terra-icon/lib/icon/IconNoSignal';
+import IconNotification from 'terra-icon/lib/icon/IconNotification';
+import IconNotificationOff from 'terra-icon/lib/icon/IconNotificationOff';
+import IconNotMet from 'terra-icon/lib/icon/IconNotMet';
+import IconOutgoingCall from 'terra-icon/lib/icon/IconOutgoingCall';
+import IconOverDue from 'terra-icon/lib/icon/IconOverDue';
+import IconPadlock from 'terra-icon/lib/icon/IconPadlock';
+import IconPanelLeft from 'terra-icon/lib/icon/IconPanelLeft';
+import IconPanelRight from 'terra-icon/lib/icon/IconPanelRight';
+import IconPaperFolded from 'terra-icon/lib/icon/IconPaperFolded';
+import IconPaperPencil from 'terra-icon/lib/icon/IconPaperPencil';
+import IconPause from 'terra-icon/lib/icon/IconPause';
+import IconPending from 'terra-icon/lib/icon/IconPending';
+import IconPerson from 'terra-icon/lib/icon/IconPerson';
+import IconPersonHospital from 'terra-icon/lib/icon/IconPersonHospital';
+import IconPersonnelPerson from 'terra-icon/lib/icon/IconPersonnelPerson';
+import IconPharmacyReject from 'terra-icon/lib/icon/IconPharmacyReject';
+import IconPharmacyReview from 'terra-icon/lib/icon/IconPharmacyReview';
+import IconPhone from 'terra-icon/lib/icon/IconPhone';
+import IconPhoneDown from 'terra-icon/lib/icon/IconPhoneDown';
+import IconPill from 'terra-icon/lib/icon/IconPill';
+import IconPinDiagonal from 'terra-icon/lib/icon/IconPinDiagonal';
+import IconPinDown from 'terra-icon/lib/icon/IconPinDown';
+import IconPrevious from 'terra-icon/lib/icon/IconPrevious';
+import IconPrinter from 'terra-icon/lib/icon/IconPrinter';
+import IconProjects from 'terra-icon/lib/icon/IconProjects';
+import IconProtocol from 'terra-icon/lib/icon/IconProtocol';
+import IconProvider from 'terra-icon/lib/icon/IconProvider';
+import IconQuestion from 'terra-icon/lib/icon/IconQuestion';
+import IconQuestionOutline from 'terra-icon/lib/icon/IconQuestionOutline';
+import IconRecurringEvent from 'terra-icon/lib/icon/IconRecurringEvent';
+import IconRefresh from 'terra-icon/lib/icon/IconRefresh';
+import IconReload from 'terra-icon/lib/icon/IconReload';
+import IconReply from 'terra-icon/lib/icon/IconReply';
+import IconReplyAll from 'terra-icon/lib/icon/IconReplyAll';
+import IconRequired from 'terra-icon/lib/icon/IconRequired';
+import IconRight from 'terra-icon/lib/icon/IconRight';
+import IconSave from 'terra-icon/lib/icon/IconSave';
+import IconScheduled from 'terra-icon/lib/icon/IconScheduled';
+import IconScratchPad from 'terra-icon/lib/icon/IconScratchPad';
+import IconSearch from 'terra-icon/lib/icon/IconSearch';
+import IconSend from 'terra-icon/lib/icon/IconSend';
+import IconSeparate from 'terra-icon/lib/icon/IconSeparate';
+import IconSettings from 'terra-icon/lib/icon/IconSettings';
+import IconSortAscending from 'terra-icon/lib/icon/IconSortAscending';
+import IconSortDescending from 'terra-icon/lib/icon/IconSortDescending';
+import IconSpinner from 'terra-icon/lib/icon/IconSpinner';
+import IconSuccess from 'terra-icon/lib/icon/IconSuccess';
+import IconSuccessInverse from 'terra-icon/lib/icon/IconSuccessInverse';
+import IconSwap from 'terra-icon/lib/icon/IconSwap';
+import IconTable from 'terra-icon/lib/icon/IconTable';
+import IconTag from 'terra-icon/lib/icon/IconTag';
+import IconTile from 'terra-icon/lib/icon/IconTile';
+import IconTrash from 'terra-icon/lib/icon/IconTrash';
+import IconTreemap from 'terra-icon/lib/icon/IconTreemap';
+import IconTrophy from 'terra-icon/lib/icon/IconTrophy';
+import IconUnavailable from 'terra-icon/lib/icon/IconUnavailable';
+import IconUnexpected from 'terra-icon/lib/icon/IconUnexpected';
+import IconUnknown from 'terra-icon/lib/icon/IconUnknown';
+import IconUnlock from 'terra-icon/lib/icon/IconUnlock';
+import IconUnread from 'terra-icon/lib/icon/IconUnread';
+import IconUnspecified from 'terra-icon/lib/icon/IconUnspecified';
+import IconUp from 'terra-icon/lib/icon/IconUp';
+import IconUpload from 'terra-icon/lib/icon/IconUpload';
+import IconUsers from 'terra-icon/lib/icon/IconUsers';
+import IconVideoCamera from 'terra-icon/lib/icon/IconVideoCamera';
+import IconVideoCameraDisabled from 'terra-icon/lib/icon/IconVideoCameraDisabled';
+import IconVisualization from 'terra-icon/lib/icon/IconVisualization';
+import IconWarning from 'terra-icon/lib/icon/IconWarning';
+import IconZoomIn from 'terra-icon/lib/icon/IconZoomIn';
+import IconZoomOut from 'terra-icon/lib/icon/IconZoomOut';
+
+const iconMap = {
+  'IconAbnormal': IconAbnormal,
+  'IconAdd': IconAdd,
+  'IconAddPerson': IconAddPerson,
+  'IconAlert': IconAlert,
+  'IconAllergy': IconAllergy,
+  'IconAnalytics': IconAnalytics,
+  'IconAnnouncement': IconAnnouncement,
+  'IconArchive': IconArchive,
+  'IconAttachment': IconAttachment,
+  'IconAudio': IconAudio,
+  'IconAvailable': IconAvailable,
+  'IconAway': IconAway,
+  'IconBookmark': IconBookmark,
+  'IconBriefcase': IconBriefcase,
+  'IconBusy': IconBusy,
+  'IconCalculator': IconCalculator,
+  'IconCalendar': IconCalendar,
+  'IconCamera': IconCamera,
+  'IconCancel': IconCancel,
+  'IconCaretDown': IconCaretDown,
+  'IconCaretLeft': IconCaretLeft,
+  'IconCaretRight': IconCaretRight,
+  'IconCaretUp': IconCaretUp,
+  'IconChecklist': IconChecklist,
+  'IconCheckmark': IconCheckmark,
+  'IconChevron': IconChevron,
+  'IconChevronDown': IconChevronDown,
+  'IconChevronLeft': IconChevronLeft,
+  'IconChevronRight': IconChevronRight,
+  'IconChevronUp': IconChevronUp,
+  'IconClear': IconClear,
+  'IconClipboard': IconClipboard,
+  'IconClock': IconClock,
+  'IconClose': IconClose,
+  'IconComment': IconComment,
+  'IconCommit': IconCommit,
+  'IconComplete': IconComplete,
+  'IconCompose': IconCompose,
+  'IconCritical': IconCritical,
+  'IconDevice': IconDevice,
+  'IconDeviceAlert': IconDeviceAlert,
+  'IconDeviceCheck': IconDeviceCheck,
+  'IconDiamond': IconDiamond,
+  'IconDocuments': IconDocuments,
+  'IconDoNotDisturb': IconDoNotDisturb,
+  'IconDoorOpen': IconDoorOpen,
+  'IconDown': IconDown,
+  'IconDownload': IconDownload,
+  'IconDue': IconDue,
+  'IconDueSoon': IconDueSoon,
+  'IconEdit': IconEdit,
+  'IconEllipses': IconEllipses,
+  'IconEnvelope': IconEnvelope,
+  'IconError': IconError,
+  'IconExclamation': IconExclamation,
+  'IconExpandLess': IconExpandLess,
+  'IconExpandMore': IconExpandMore,
+  'IconFeatured': IconFeatured,
+  'IconFeaturedOff': IconFeaturedOff,
+  'IconFeaturedOutline': IconFeaturedOutline,
+  'IconFirst': IconFirst,
+  'IconFlag': IconFlag,
+  'IconFlowsheet': IconFlowsheet,
+  'IconFolder': IconFolder,
+  'IconForward': IconForward,
+  'IconFunnel': IconFunnel,
+  'IconGapChecking': IconGapChecking,
+  'IconGlasses': IconGlasses,
+  'IconHelp': IconHelp,
+  'IconHelpInverse': IconHelpInverse,
+  'IconHigh': IconHigh,
+  'IconHighPriority': IconHighPriority,
+  'IconHospital': IconHospital,
+  'IconHouse': IconHouse,
+  'IconImage': IconImage,
+  'IconImplant': IconImplant,
+  'IconIncomingCall': IconIncomingCall,
+  'IconIncomplete': IconIncomplete,
+  'IconInformation': IconInformation,
+  'IconInformationInverse': IconInformationInverse,
+  'IconIPass': IconIPass,
+  'IconItalicI': IconItalicI,
+  'IconKnurling': IconKnurling,
+  'IconLast': IconLast,
+  'IconLeft': IconLeft,
+  'IconLeftPane': IconLeftPane,
+  'IconLightbulb': IconLightbulb,
+  'IconLink': IconLink,
+  'IconList': IconList,
+  'IconLookback': IconLookback,
+  'IconLow': IconLow,
+  'IconMax': IconMax,
+  'IconMaximize': IconMaximize,
+  'IconMenu': IconMenu,
+  'IconMerge': IconMerge,
+  'IconMicrophone': IconMicrophone,
+  'IconMicrophoneDisabled': IconMicrophoneDisabled,
+  'IconMin': IconMin,
+  'IconMinimize': IconMinimize,
+  'IconMinus': IconMinus,
+  'IconMissedCall': IconMissedCall,
+  'IconModified': IconModified,
+  'IconNext': IconNext,
+  'IconNoSignal': IconNoSignal,
+  'IconNotification': IconNotification,
+  'IconNotificationOff': IconNotificationOff,
+  'IconNotMet': IconNotMet,
+  'IconOutgoingCall': IconOutgoingCall,
+  'IconOverDue': IconOverDue,
+  'IconPadlock': IconPadlock,
+  'IconPanelLeft': IconPanelLeft,
+  'IconPanelRight': IconPanelRight,
+  'IconPaperFolded': IconPaperFolded,
+  'IconPaperPencil': IconPaperPencil,
+  'IconPause': IconPause,
+  'IconPending': IconPending,
+  'IconPerson': IconPerson,
+  'IconPersonHospital': IconPersonHospital,
+  'IconPersonnelPerson': IconPersonnelPerson,
+  'IconPharmacyReject': IconPharmacyReject,
+  'IconPharmacyReview': IconPharmacyReview,
+  'IconPhone': IconPhone,
+  'IconPhoneDown': IconPhoneDown,
+  'IconPill': IconPill,
+  'IconPinDiagonal': IconPinDiagonal,
+  'IconPinDown': IconPinDown,
+  'IconPrevious': IconPrevious,
+  'IconPrinter': IconPrinter,
+  'IconProjects': IconProjects,
+  'IconProtocol': IconProtocol,
+  'IconProvider': IconProvider,
+  'IconQuestion': IconQuestion,
+  'IconQuestionOutline': IconQuestionOutline,
+  'IconRecurringEvent': IconRecurringEvent,
+  'IconRefresh': IconRefresh,
+  'IconReload': IconReload,
+  'IconReply': IconReply,
+  'IconReplyAll': IconReplyAll,
+  'IconRequired': IconRequired,
+  'IconRight': IconRight,
+  'IconSave': IconSave,
+  'IconScheduled': IconScheduled,
+  'IconScratchPad': IconScratchPad,
+  'IconSearch': IconSearch,
+  'IconSend': IconSend,
+  'IconSeparate': IconSeparate,
+  'IconSettings': IconSettings,
+  'IconSortAscending': IconSortAscending,
+  'IconSortDescending': IconSortDescending,
+  'IconSpinner': IconSpinner,
+  'IconSuccess': IconSuccess,
+  'IconSuccessInverse': IconSuccessInverse,
+  'IconSwap': IconSwap,
+  'IconTable': IconTable,
+  'IconTag': IconTag,
+  'IconTile': IconTile,
+  'IconTrash': IconTrash,
+  'IconTreemap': IconTreemap,
+  'IconTrophy': IconTrophy,
+  'IconUnavailable': IconUnavailable,
+  'IconUnexpected': IconUnexpected,
+  'IconUnknown': IconUnknown,
+  'IconUnlock': IconUnlock,
+  'IconUnread': IconUnread,
+  'IconUnspecified': IconUnspecified,
+  'IconUp': IconUp,
+  'IconUpload': IconUpload,
+  'IconUsers': IconUsers,
+  'IconVideoCamera': IconVideoCamera,
+  'IconVideoCameraDisabled': IconVideoCameraDisabled,
+  'IconVisualization': IconVisualization,
+  'IconWarning': IconWarning,
+  'IconZoomIn': IconZoomIn,
+  'IconZoomOut': IconZoomOut,
+};
+
+export default iconMap;
