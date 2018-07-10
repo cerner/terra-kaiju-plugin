e4b46e70068b632f067abbb36a114d7cef1c6f7b
Esser,Stephen <Stephen.Esser@Cerner.com>
2018-07-10 10:59:51 -0500

    Added - New Icons

--- iconMap.js
+++ iconMap.js
@@ -1,7 +1,9 @@
 import IconAbnormal from 'terra-icon/lib/icon/IconAbnormal';
+import IconAbnormalLowLight from 'terra-icon/lib/icon/IconAbnormalLowLight';
 import IconAdd from 'terra-icon/lib/icon/IconAdd';
 import IconAddPerson from 'terra-icon/lib/icon/IconAddPerson';
 import IconAlert from 'terra-icon/lib/icon/IconAlert';
+import IconAlertLowLight from 'terra-icon/lib/icon/IconAlertLowLight';
 import IconAllergy from 'terra-icon/lib/icon/IconAllergy';
 import IconAnalytics from 'terra-icon/lib/icon/IconAnalytics';
 import IconAnnouncement from 'terra-icon/lib/icon/IconAnnouncement';
@@ -11,6 +13,7 @@ import IconAudio from 'terra-icon/lib/icon/IconAudio';
 import IconAvailable from 'terra-icon/lib/icon/IconAvailable';
 import IconAway from 'terra-icon/lib/icon/IconAway';
 import IconBedAssigned from 'terra-icon/lib/icon/IconBedAssigned';
+import IconBedAssignedLowLight from 'terra-icon/lib/icon/IconBedAssignedLowLight';
 import IconBedRequested from 'terra-icon/lib/icon/IconBedRequested';
 import IconBookmark from 'terra-icon/lib/icon/IconBookmark';
 import IconBriefcase from 'terra-icon/lib/icon/IconBriefcase';
@@ -30,43 +33,63 @@ import IconChevronDown from 'terra-icon/lib/icon/IconChevronDown';
 import IconChevronLeft from 'terra-icon/lib/icon/IconChevronLeft';
 import IconChevronRight from 'terra-icon/lib/icon/IconChevronRight';
 import IconChevronUp from 'terra-icon/lib/icon/IconChevronUp';
+import IconCircleSymbol from 'terra-icon/lib/icon/IconCircleSymbol';
 import IconClear from 'terra-icon/lib/icon/IconClear';
 import IconClipboard from 'terra-icon/lib/icon/IconClipboard';
 import IconClock from 'terra-icon/lib/icon/IconClock';
 import IconClose from 'terra-icon/lib/icon/IconClose';
+import IconCollapseRow from 'terra-icon/lib/icon/IconCollapseRow';
 import IconComment from 'terra-icon/lib/icon/IconComment';
 import IconCommit from 'terra-icon/lib/icon/IconCommit';
 import IconComplete from 'terra-icon/lib/icon/IconComplete';
 import IconCompose from 'terra-icon/lib/icon/IconCompose';
 import IconCritical from 'terra-icon/lib/icon/IconCritical';
 import IconCriticalInverse from 'terra-icon/lib/icon/IconCriticalInverse';
+import IconCriticalInverseLowLight from 'terra-icon/lib/icon/IconCriticalInverseLowLight';
+import IconCriticalLowLight from 'terra-icon/lib/icon/IconCriticalLowLight';
 import IconDevice from 'terra-icon/lib/icon/IconDevice';
 import IconDeviceAlert from 'terra-icon/lib/icon/IconDeviceAlert';
+import IconDeviceAlertLowLight from 'terra-icon/lib/icon/IconDeviceAlertLowLight';
 import IconDeviceCheck from 'terra-icon/lib/icon/IconDeviceCheck';
+import IconDeviceCheckLowLight from 'terra-icon/lib/icon/IconDeviceCheckLowLight';
 import IconDiamond from 'terra-icon/lib/icon/IconDiamond';
+import IconDiamondLowLight from 'terra-icon/lib/icon/IconDiamondLowLight';
+import IconDiamondSymbol from 'terra-icon/lib/icon/IconDiamondSymbol';
 import IconDischargeComingDue from 'terra-icon/lib/icon/IconDischargeComingDue';
+import IconDischargeComingDueLowLight from 'terra-icon/lib/icon/IconDischargeComingDueLowLight';
 import IconDischargeComplete from 'terra-icon/lib/icon/IconDischargeComplete';
+import IconDischargeCompleteLowLight from 'terra-icon/lib/icon/IconDischargeCompleteLowLight';
 import IconDischargeOverDue from 'terra-icon/lib/icon/IconDischargeOverDue';
+import IconDischargeOverDueLowLight from 'terra-icon/lib/icon/IconDischargeOverDueLowLight';
 import IconDocuments from 'terra-icon/lib/icon/IconDocuments';
 import IconDoNotDisturb from 'terra-icon/lib/icon/IconDoNotDisturb';
 import IconDoorOpen from 'terra-icon/lib/icon/IconDoorOpen';
+import IconDoubleChevronLeft from 'terra-icon/lib/icon/IconDoubleChevronLeft';
+import IconDoubleChevronRight from 'terra-icon/lib/icon/IconDoubleChevronRight';
 import IconDown from 'terra-icon/lib/icon/IconDown';
 import IconDownload from 'terra-icon/lib/icon/IconDownload';
+import IconDrugDrug from 'terra-icon/lib/icon/IconDrugDrug';
+import IconDrugDrugLowLight from 'terra-icon/lib/icon/IconDrugDrugLowLight';
 import IconDue from 'terra-icon/lib/icon/IconDue';
 import IconDueSoon from 'terra-icon/lib/icon/IconDueSoon';
+import IconDueSoonLowLight from 'terra-icon/lib/icon/IconDueSoonLowLight';
 import IconEdit from 'terra-icon/lib/icon/IconEdit';
 import IconEllipses from 'terra-icon/lib/icon/IconEllipses';
 import IconEnvelope from 'terra-icon/lib/icon/IconEnvelope';
 import IconError from 'terra-icon/lib/icon/IconError';
 import IconErrorDark from 'terra-icon/lib/icon/IconErrorDark';
+import IconErrorLowLight from 'terra-icon/lib/icon/IconErrorLowLight';
 import IconExclamation from 'terra-icon/lib/icon/IconExclamation';
 import IconExpandLess from 'terra-icon/lib/icon/IconExpandLess';
 import IconExpandMore from 'terra-icon/lib/icon/IconExpandMore';
+import IconExpandRow from 'terra-icon/lib/icon/IconExpandRow';
 import IconExternalLink from 'terra-icon/lib/icon/IconExternalLink';
 import IconFeatured from 'terra-icon/lib/icon/IconFeatured';
 import IconFeaturedOff from 'terra-icon/lib/icon/IconFeaturedOff';
 import IconFeaturedOutline from 'terra-icon/lib/icon/IconFeaturedOutline';
 import IconFirst from 'terra-icon/lib/icon/IconFirst';
+import IconFitScale from 'terra-icon/lib/icon/IconFitScale';
+import IconFitWidth from 'terra-icon/lib/icon/IconFitWidth';
 import IconFlag from 'terra-icon/lib/icon/IconFlag';
 import IconFlipHorizontal from 'terra-icon/lib/icon/IconFlipHorizontal';
 import IconFlipVertical from 'terra-icon/lib/icon/IconFlipVertical';
@@ -76,10 +99,16 @@ import IconForward from 'terra-icon/lib/icon/IconForward';
 import IconFunnel from 'terra-icon/lib/icon/IconFunnel';
 import IconGapChecking from 'terra-icon/lib/icon/IconGapChecking';
 import IconGlasses from 'terra-icon/lib/icon/IconGlasses';
+import IconHalfRating from 'terra-icon/lib/icon/IconHalfRating';
 import IconHelp from 'terra-icon/lib/icon/IconHelp';
 import IconHelpInverse from 'terra-icon/lib/icon/IconHelpInverse';
+import IconHelpInverseLowLight from 'terra-icon/lib/icon/IconHelpInverseLowLight';
+import IconHelpLowLight from 'terra-icon/lib/icon/IconHelpLowLight';
+import IconHide from 'terra-icon/lib/icon/IconHide';
 import IconHigh from 'terra-icon/lib/icon/IconHigh';
 import IconHighInverse from 'terra-icon/lib/icon/IconHighInverse';
+import IconHighInverseLowLight from 'terra-icon/lib/icon/IconHighInverseLowLight';
+import IconHighLowLight from 'terra-icon/lib/icon/IconHighLowLight';
 import IconHighPriority from 'terra-icon/lib/icon/IconHighPriority';
 import IconHold from 'terra-icon/lib/icon/IconHold';
 import IconHospital from 'terra-icon/lib/icon/IconHospital';
@@ -88,8 +117,12 @@ import IconImage from 'terra-icon/lib/icon/IconImage';
 import IconImplant from 'terra-icon/lib/icon/IconImplant';
 import IconIncomingCall from 'terra-icon/lib/icon/IconIncomingCall';
 import IconIncomplete from 'terra-icon/lib/icon/IconIncomplete';
+import IconIncompleteInverse from 'terra-icon/lib/icon/IconIncompleteInverse';
+import IconIncompleteInverseLowLight from 'terra-icon/lib/icon/IconIncompleteInverseLowLight';
 import IconInformation from 'terra-icon/lib/icon/IconInformation';
 import IconInformationInverse from 'terra-icon/lib/icon/IconInformationInverse';
+import IconInformationInverseLowLight from 'terra-icon/lib/icon/IconInformationInverseLowLight';
+import IconInformationLowLight from 'terra-icon/lib/icon/IconInformationLowLight';
 import IconIPass from 'terra-icon/lib/icon/IconIPass';
 import IconItalicI from 'terra-icon/lib/icon/IconItalicI';
 import IconKnurling from 'terra-icon/lib/icon/IconKnurling';
@@ -101,6 +134,9 @@ import IconLink from 'terra-icon/lib/icon/IconLink';
 import IconList from 'terra-icon/lib/icon/IconList';
 import IconLookback from 'terra-icon/lib/icon/IconLookback';
 import IconLow from 'terra-icon/lib/icon/IconLow';
+import IconLowLowLight from 'terra-icon/lib/icon/IconLowLowLight';
+import IconLowRisk from 'terra-icon/lib/icon/IconLowRisk';
+import IconLowRiskLowLight from 'terra-icon/lib/icon/IconLowRiskLowLight';
 import IconMax from 'terra-icon/lib/icon/IconMax';
 import IconMaximize from 'terra-icon/lib/icon/IconMaximize';
 import IconMenu from 'terra-icon/lib/icon/IconMenu';
@@ -112,32 +148,38 @@ import IconMinimize from 'terra-icon/lib/icon/IconMinimize';
 import IconMinus from 'terra-icon/lib/icon/IconMinus';
 import IconMissedCall from 'terra-icon/lib/icon/IconMissedCall';
 import IconModerate from 'terra-icon/lib/icon/IconModerate';
+import IconModerateLowLight from 'terra-icon/lib/icon/IconModerateLowLight';
 import IconModified from 'terra-icon/lib/icon/IconModified';
 import IconNext from 'terra-icon/lib/icon/IconNext';
 import IconNoRisk from 'terra-icon/lib/icon/IconNoRisk';
+import IconNoRiskLowLight from 'terra-icon/lib/icon/IconNoRiskLowLight';
 import IconNoSignal from 'terra-icon/lib/icon/IconNoSignal';
 import IconNotification from 'terra-icon/lib/icon/IconNotification';
 import IconNotificationOff from 'terra-icon/lib/icon/IconNotificationOff';
 import IconNotMet from 'terra-icon/lib/icon/IconNotMet';
 import IconOutgoingCall from 'terra-icon/lib/icon/IconOutgoingCall';
 import IconOverDue from 'terra-icon/lib/icon/IconOverDue';
+import IconOverDueLowLight from 'terra-icon/lib/icon/IconOverDueLowLight';
 import IconPadlock from 'terra-icon/lib/icon/IconPadlock';
 import IconPanelLeft from 'terra-icon/lib/icon/IconPanelLeft';
 import IconPanelRight from 'terra-icon/lib/icon/IconPanelRight';
 import IconPaperFolded from 'terra-icon/lib/icon/IconPaperFolded';
 import IconPaperPencil from 'terra-icon/lib/icon/IconPaperPencil';
+import IconPartiallyMet from 'terra-icon/lib/icon/IconPartiallyMet';
 import IconPause from 'terra-icon/lib/icon/IconPause';
 import IconPending from 'terra-icon/lib/icon/IconPending';
 import IconPerson from 'terra-icon/lib/icon/IconPerson';
 import IconPersonHospital from 'terra-icon/lib/icon/IconPersonHospital';
 import IconPersonnelPerson from 'terra-icon/lib/icon/IconPersonnelPerson';
 import IconPharmacyReject from 'terra-icon/lib/icon/IconPharmacyReject';
+import IconPharmacyRejectLowLight from 'terra-icon/lib/icon/IconPharmacyRejectLowLight';
 import IconPharmacyReview from 'terra-icon/lib/icon/IconPharmacyReview';
 import IconPhone from 'terra-icon/lib/icon/IconPhone';
 import IconPhoneDown from 'terra-icon/lib/icon/IconPhoneDown';
 import IconPill from 'terra-icon/lib/icon/IconPill';
 import IconPinDiagonal from 'terra-icon/lib/icon/IconPinDiagonal';
 import IconPinDown from 'terra-icon/lib/icon/IconPinDown';
+import IconPlusSymbol from 'terra-icon/lib/icon/IconPlusSymbol';
 import IconPrevious from 'terra-icon/lib/icon/IconPrevious';
 import IconPrinter from 'terra-icon/lib/icon/IconPrinter';
 import IconProjects from 'terra-icon/lib/icon/IconProjects';
@@ -145,6 +187,7 @@ import IconProtocol from 'terra-icon/lib/icon/IconProtocol';
 import IconProvider from 'terra-icon/lib/icon/IconProvider';
 import IconQuestion from 'terra-icon/lib/icon/IconQuestion';
 import IconQuestionOutline from 'terra-icon/lib/icon/IconQuestionOutline';
+import IconRectangleSymbol from 'terra-icon/lib/icon/IconRectangleSymbol';
 import IconRecurringEvent from 'terra-icon/lib/icon/IconRecurringEvent';
 import IconRefresh from 'terra-icon/lib/icon/IconRefresh';
 import IconReload from 'terra-icon/lib/icon/IconReload';
@@ -152,29 +195,42 @@ import IconReply from 'terra-icon/lib/icon/IconReply';
 import IconReplyAll from 'terra-icon/lib/icon/IconReplyAll';
 import IconRequired from 'terra-icon/lib/icon/IconRequired';
 import IconRight from 'terra-icon/lib/icon/IconRight';
+import IconRollup from 'terra-icon/lib/icon/IconRollup';
 import IconRotateLeft from 'terra-icon/lib/icon/IconRotateLeft';
 import IconRotateRight from 'terra-icon/lib/icon/IconRotateRight';
 import IconSave from 'terra-icon/lib/icon/IconSave';
 import IconScheduled from 'terra-icon/lib/icon/IconScheduled';
+import IconScheduledLowLight from 'terra-icon/lib/icon/IconScheduledLowLight';
 import IconScratchPad from 'terra-icon/lib/icon/IconScratchPad';
 import IconSearch from 'terra-icon/lib/icon/IconSearch';
+import IconSecondaryProvider from 'terra-icon/lib/icon/IconSecondaryProvider';
 import IconSend from 'terra-icon/lib/icon/IconSend';
 import IconSeparate from 'terra-icon/lib/icon/IconSeparate';
 import IconSettings from 'terra-icon/lib/icon/IconSettings';
+import IconSimulation from 'terra-icon/lib/icon/IconSimulation';
 import IconSortAscending from 'terra-icon/lib/icon/IconSortAscending';
 import IconSortDescending from 'terra-icon/lib/icon/IconSortDescending';
 import IconSpinner from 'terra-icon/lib/icon/IconSpinner';
+import IconSquareSymbol from 'terra-icon/lib/icon/IconSquareSymbol';
 import IconSuccess from 'terra-icon/lib/icon/IconSuccess';
 import IconSuccessInverse from 'terra-icon/lib/icon/IconSuccessInverse';
+import IconSuccessInverseLowLight from 'terra-icon/lib/icon/IconSuccessInverseLowLight';
+import IconSuccessLowLight from 'terra-icon/lib/icon/IconSuccessLowLight';
 import IconSwap from 'terra-icon/lib/icon/IconSwap';
 import IconTable from 'terra-icon/lib/icon/IconTable';
 import IconTag from 'terra-icon/lib/icon/IconTag';
+import IconTeardropSymbol from 'terra-icon/lib/icon/IconTeardropSymbol';
+import IconTeardropUpSymbol from 'terra-icon/lib/icon/IconTeardropUpSymbol';
+import IconThinDiamondSymbol from 'terra-icon/lib/icon/IconThinDiamondSymbol';
 import IconTile from 'terra-icon/lib/icon/IconTile';
 import IconTrash from 'terra-icon/lib/icon/IconTrash';
 import IconTreemap from 'terra-icon/lib/icon/IconTreemap';
+import IconTriangleDownSymbol from 'terra-icon/lib/icon/IconTriangleDownSymbol';
+import IconTriangleSymbol from 'terra-icon/lib/icon/IconTriangleSymbol';
 import IconTrophy from 'terra-icon/lib/icon/IconTrophy';
 import IconUnavailable from 'terra-icon/lib/icon/IconUnavailable';
 import IconUnexpected from 'terra-icon/lib/icon/IconUnexpected';
+import IconUnexpectedLowLight from 'terra-icon/lib/icon/IconUnexpectedLowLight';
 import IconUnknown from 'terra-icon/lib/icon/IconUnknown';
 import IconUnlock from 'terra-icon/lib/icon/IconUnlock';
 import IconUnread from 'terra-icon/lib/icon/IconUnread';
@@ -186,14 +242,18 @@ import IconVideoCamera from 'terra-icon/lib/icon/IconVideoCamera';
 import IconVideoCameraDisabled from 'terra-icon/lib/icon/IconVideoCameraDisabled';
 import IconVisualization from 'terra-icon/lib/icon/IconVisualization';
 import IconWarning from 'terra-icon/lib/icon/IconWarning';
+import IconWarningLowLight from 'terra-icon/lib/icon/IconWarningLowLight';
+import IconXSymbol from 'terra-icon/lib/icon/IconXSymbol';
 import IconZoomIn from 'terra-icon/lib/icon/IconZoomIn';
 import IconZoomOut from 'terra-icon/lib/icon/IconZoomOut';
 
 const iconMap = {
   IconAbnormal,
+  IconAbnormalLowLight,
   IconAdd,
   IconAddPerson,
   IconAlert,
+  IconAlertLowLight,
   IconAllergy,
   IconAnalytics,
   IconAnnouncement,
@@ -203,6 +263,7 @@ const iconMap = {
   IconAvailable,
   IconAway,
   IconBedAssigned,
+  IconBedAssignedLowLight,
   IconBedRequested,
   IconBookmark,
   IconBriefcase,
@@ -222,43 +283,63 @@ const iconMap = {
   IconChevronLeft,
   IconChevronRight,
   IconChevronUp,
+  IconCircleSymbol,
   IconClear,
   IconClipboard,
   IconClock,
   IconClose,
+  IconCollapseRow,
   IconComment,
   IconCommit,
   IconComplete,
   IconCompose,
   IconCritical,
   IconCriticalInverse,
+  IconCriticalInverseLowLight,
+  IconCriticalLowLight,
   IconDevice,
   IconDeviceAlert,
+  IconDeviceAlertLowLight,
   IconDeviceCheck,
+  IconDeviceCheckLowLight,
   IconDiamond,
+  IconDiamondLowLight,
+  IconDiamondSymbol,
   IconDischargeComingDue,
+  IconDischargeComingDueLowLight,
   IconDischargeComplete,
+  IconDischargeCompleteLowLight,
   IconDischargeOverDue,
+  IconDischargeOverDueLowLight,
   IconDocuments,
   IconDoNotDisturb,
   IconDoorOpen,
+  IconDoubleChevronLeft,
+  IconDoubleChevronRight,
   IconDown,
   IconDownload,
+  IconDrugDrug,
+  IconDrugDrugLowLight,
   IconDue,
   IconDueSoon,
+  IconDueSoonLowLight,
   IconEdit,
   IconEllipses,
   IconEnvelope,
   IconError,
   IconErrorDark,
+  IconErrorLowLight,
   IconExclamation,
   IconExpandLess,
   IconExpandMore,
+  IconExpandRow,
   IconExternalLink,
   IconFeatured,
   IconFeaturedOff,
   IconFeaturedOutline,
   IconFirst,
+  IconFitScale,
+  IconFitWidth,
   IconFlag,
   IconFlipHorizontal,
   IconFlipVertical,
@@ -268,10 +349,16 @@ const iconMap = {
   IconFunnel,
   IconGapChecking,
   IconGlasses,
+  IconHalfRating,
   IconHelp,
   IconHelpInverse,
+  IconHelpInverseLowLight,
+  IconHelpLowLight,
+  IconHide,
   IconHigh,
   IconHighInverse,
+  IconHighInverseLowLight,
+  IconHighLowLight,
   IconHighPriority,
   IconHold,
   IconHospital,
@@ -280,8 +367,12 @@ const iconMap = {
   IconImplant,
   IconIncomingCall,
   IconIncomplete,
+  IconIncompleteInverse,
+  IconIncompleteInverseLowLight,
   IconInformation,
   IconInformationInverse,
+  IconInformationInverseLowLight,
+  IconInformationLowLight,
   IconIPass,
   IconItalicI,
   IconKnurling,
@@ -293,6 +384,9 @@ const iconMap = {
   IconList,
   IconLookback,
   IconLow,
+  IconLowLowLight,
+  IconLowRisk,
+  IconLowRiskLowLight,
   IconMax,
   IconMaximize,
   IconMenu,
@@ -304,32 +398,38 @@ const iconMap = {
   IconMinus,
   IconMissedCall,
   IconModerate,
+  IconModerateLowLight,
   IconModified,
   IconNext,
   IconNoRisk,
+  IconNoRiskLowLight,
   IconNoSignal,
   IconNotification,
   IconNotificationOff,
   IconNotMet,
   IconOutgoingCall,
   IconOverDue,
+  IconOverDueLowLight,
   IconPadlock,
   IconPanelLeft,
   IconPanelRight,
   IconPaperFolded,
   IconPaperPencil,
+  IconPartiallyMet,
   IconPause,
   IconPending,
   IconPerson,
   IconPersonHospital,
   IconPersonnelPerson,
   IconPharmacyReject,
+  IconPharmacyRejectLowLight,
   IconPharmacyReview,
   IconPhone,
   IconPhoneDown,
   IconPill,
   IconPinDiagonal,
   IconPinDown,
+  IconPlusSymbol,
   IconPrevious,
   IconPrinter,
   IconProjects,
@@ -337,6 +437,7 @@ const iconMap = {
   IconProvider,
   IconQuestion,
   IconQuestionOutline,
+  IconRectangleSymbol,
   IconRecurringEvent,
   IconRefresh,
   IconReload,
@@ -344,29 +445,42 @@ const iconMap = {
   IconReplyAll,
   IconRequired,
   IconRight,
+  IconRollup,
   IconRotateLeft,
   IconRotateRight,
   IconSave,
   IconScheduled,
+  IconScheduledLowLight,
   IconScratchPad,
   IconSearch,
+  IconSecondaryProvider,
   IconSend,
   IconSeparate,
   IconSettings,
+  IconSimulation,
   IconSortAscending,
   IconSortDescending,
   IconSpinner,
+  IconSquareSymbol,
   IconSuccess,
   IconSuccessInverse,
+  IconSuccessInverseLowLight,
+  IconSuccessLowLight,
   IconSwap,
   IconTable,
   IconTag,
+  IconTeardropSymbol,
+  IconTeardropUpSymbol,
+  IconThinDiamondSymbol,
   IconTile,
   IconTrash,
   IconTreemap,
+  IconTriangleDownSymbol,
+  IconTriangleSymbol,
   IconTrophy,
   IconUnavailable,
   IconUnexpected,
+  IconUnexpectedLowLight,
   IconUnknown,
   IconUnlock,
   IconUnread,
@@ -378,6 +492,8 @@ const iconMap = {
   IconVideoCameraDisabled,
   IconVisualization,
   IconWarning,
+  IconWarningLowLight,
+  IconXSymbol,
   IconZoomIn,
   IconZoomOut,
 };
